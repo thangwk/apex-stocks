a94a2e923cbb7aa3870f631c1f74108fc0e88f50
Josiah <thangwk@hotmail.com>
2026-03-05 10:49:22 +0800

    two-way watchlist sync between web app and Telegram

---
+++ api/watchlist-load.js
@@ -0,0 +1,11 @@
+import { getWatchlist } from './_redis.js';
+
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') return res.status(200).end();
+
+  const tickers = await getWatchlist();
+  res.status(200).json({ tickers });
+}
